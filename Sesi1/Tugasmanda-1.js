9a39e17f7a7ab1e0c5c1c3b97254eb96a1d95ae5
nursafitriamanda <114926923+nursafitriamanda@users.noreply.github.com>
2023-09-29 11:29:12 +0700

    Add files via upload

---
+++ Sesi1/Tugasmanda-1.js
@@ -0,0 +1,20 @@
+function dataMahasiswa(parm1, parm2) {
+    let result = (parm1 + parm2)/2
+    return result
+}
+
+let namaMahasiswa = "Amanda Nursafitri"
+let kelas = "TI 22 H"
+let jurusan = "Teknik Informatika"
+let universitas = "Nusa Putra"
+let NIM = 20220040093
+let ipkPertama = 4
+let ipkKedua = 4
+
+let totalIpk = dataMahasiswa(ipkPertama, ipkKedua)
+
+console.log(`Nama: ${namaMahasiswa}`);
+console.log(`Universitas: ${universitas}`);
+console.log(`Jurusan: ${jurusan}`);
+console.log(`NIM: ${NIM}`);
+console.log(`IPK: ${totalIpk}`);
