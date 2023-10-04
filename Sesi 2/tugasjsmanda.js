a3ad3e3236ba95aadcaf058ff0c8673ac2ac988a
nursafitriamanda <114926923+nursafitriamanda@users.noreply.github.com>
2023-10-04 15:50:59 +0700

    Add files via upload

---
+++ Sesi 2/tugasjsmanda.js
@@ -0,0 +1,8 @@
+let firstName = "Amanda";
+let lastName = "Nursafitri";
+let age = 19;
+let isMarried = false;
+
+console.log(`Nama Lengkap : ${firstName} ${lastName}`);
+console.log(`Umur : ${age}`);
+console.log(`Is Married : ${isMarried}`);
